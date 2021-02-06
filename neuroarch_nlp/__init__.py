867a9d0498e0620634c9cfa9fc3ace8797c02f9f
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-02-05 21:41:23 -0500

    bump version to 0.3.0

--- neuroarch_nlp/__init__.py
+++ neuroarch_nlp/__init__.py
@@ -1,5 +1,12 @@
 #from neuroarch_nlp.interface import Translator
 
+# Ignore all exceptions so that this doesn't cause package installation
+# to fail if pkg_resources can't find neurokernel:
+try:
+    from version import __version__
+except:
+    pass
+
 import logging
 def set_loglevel( level=logging.INFO ):
     log = logging.getLogger( 'neuroarch_nlp' )
